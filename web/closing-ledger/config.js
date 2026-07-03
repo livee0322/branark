967824ddc02bcc6a8241155b2644b1ec413d226c
livee0322 <dlxodnd6564@naver.com>
2026-07-03 16:24:17 +0900

    feat: add closing ledger page runtime config

---
+++ web/closing-ledger/config.js
@@ -0,0 +1,5 @@
+window.BRANARK_CLOSING_LEDGER_CONFIG = {
+  // Apps Script 웹 앱 배포 후 받은 /exec URL을 한 번만 입력합니다.
+  // 예: https://script.google.com/macros/s/AKfycb.../exec
+  appsScriptWebAppUrl: '',
+};

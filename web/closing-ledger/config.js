18325e6e36ebe2bd88202b714e208c7604f99ff0
livee0322 <dlxodnd6564@naver.com>
2026-07-03 21:10:07 +0900

    fix: update Apps Script web app URL after redeploy

--- web/closing-ledger/config.js
+++ web/closing-ledger/config.js
@@ -1,16 +1,14 @@
 window.BRANARK_CLOSING_LEDGER_CONFIG = {
-  appsScriptWebAppUrl: 'https://script.google.com/macros/s/AKfycbz86pVCWFnXmHLjr6d8Cm2HCgF8MrG33903-ivG3UbHTcfCc8xpNuSj7Aa91m15NOeo2Q/exec',
+  appsScriptWebAppUrl: 'https://script.google.com/macros/s/AKfycbzDTBCKXytRLQnprpMnVhlsrrLnis7NFfJUTmU-oFZocpztR2W6vUiSzAx921DAH1AuyQ/exec',
   maxFileSizeBytes: 8 * 1024 * 1024,
   healthTimeoutMs: 15000,
   requestTimeoutMs: 180000,
 };
 
-window.setTimeout(() => {
-  if (document.querySelector('script[data-branark-upload-patch="true"]')) {
-    return;
-  }
-  const patchScript = document.createElement('script');
-  patchScript.src = './web/closing-ledger/upload-patch.js?v=20260703-7';
+window.setTimeout(function () {
+  if (document.querySelector('script[data-branark-upload-patch="true"]')) return;
+  var patchScript = document.createElement('scr' + 'ipt');
+  patchScript.src = './web/closing-ledger/upload-patch.js?v=20260703-8';
   patchScript.dataset.branarkUploadPatch = 'true';
   document.body.appendChild(patchScript);
 }, 1000);
